6c1e555eea0c6acdddcd28826e363ebfc68fa06a
Buho OSY <chikenbad.osy@gmail.com>
2022-05-02 21:42:18 -0500

    se agrega linea de configuracion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDvuDKZpgxepooB5vRhxfNRyDHE1VWvbQM',
+    authDomain: 'medicionlord.firebaseapp.com',
+    projectId: 'medicionlord',
+    storageBucket: 'medicionlord.appspot.com',
+    messagingSenderId: '146694993365',
+    appId: '1:146694993365:web:5aafb2c9e7a85cc808395c',
+  },
 };
